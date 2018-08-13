b1a7423e05db122a02f43aa3473a089b28a6dc64
puan0601 <anton.pugach@gmail.com>
2018-08-13 00:05:11 -0700

    balance, spent, received renders to page

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,27 +9,36 @@ class App extends Component {
 
     this.state = {
       address: undefined,
-      data: undefined
+      data: undefined,
+      txs: []
     };
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    const addr = this.element.value;
+    const addr = this.element.value; //hacky ref way to get value of the input element without making it controlled
     const baseURL = "https://blockchain.info/multiaddr?active=";
-    const URL = `${baseURL}${addr}&cors=true`;
+    const URL = `${baseURL}${addr}&cors=true`; //enabling cors allows it to work 
 
     fetch(URL)
-      .then(resp => resp.json())
-      .then(data => console.log(data))
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
         
-        // this.setState({
-        // address: this.element.value
-        // });
-      
+        this.setState({
+          address: this.element.value,
+          data
+        });
+    });
   }
-
+  
   render() {
+    if (this.state.data) {
+       const balance = this.state.data.wallet.total_balance;
+       const sent = this.state.data.wallet.total_sent;
+       const received = this.state.data.wallet.total_received;
+      }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -37,13 +46,16 @@ class App extends Component {
             <h1 className="App-title">Blockchain Bitcoin Transactions for: {this.state.address}</h1> 
           : <h1 className="App-title">Blockchain Bitcoin Transactions</h1>}
         </header>
-        <p>Enter Bitcoin address below</p>
+        <p>Enter valid Bitcoin address below</p>
         <form onSubmit={(e) => this.handleSubmit(e)}>
           <label htmlFor="bitcoin-address"> 
-            <input type="text" name="bitcoin-address" ref={(el) => this.element = el} />
+            <input type="text" name="bitcoin-address" ref={(el) => this.element = el} />{/* to instantiate the ref on the element*/}
           </label>
           <input type="submit" value="Click to Submit" />
         </form>
+          {this.state.data && <p>Balance: {this.state.data.wallet.final_balance}</p>}
+          {this.state.data && <p>Received: {this.state.data.wallet.total_received}</p>}
+          {this.state.data && <p>Spent: {this.state.data.wallet.total_sent}</p>}
       </div>
     );
   }
