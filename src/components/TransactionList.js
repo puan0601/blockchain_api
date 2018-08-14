2669b0b091a2b2031bd9a15816f40eaa28b8a6ae
puan0601 <anton.pugach@gmail.com>
2018-08-13 23:47:13 -0700

    removes console.log from TransactionList

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Transaction from './Transaction';
 
 const TransactionList = (props) => {
-    console.log(`txs props: ${props}`);
     const transItems = props.trans.map((item, idx) => {
         return <Transaction hash={item.hash} total={item.result} fee={item.fee} key={idx} />;
     });
